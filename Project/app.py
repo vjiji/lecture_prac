bd6ddddb76c9bd5f428456df18d4b8c30a5ff92c
vjiji <restmi0711@gmail.com>
2024-01-09 21:08:26 +0900

    add deploy project

--- Project/app.py
+++ Project/app.py
@@ -1,36 +1,73 @@
-# 필수 라이브러리
-'''
-0. Flask : 웹서버를 시작할 수 있는 기능. app이라는 이름으로 플라스크를 시작한다
-1. render_template : html파일을 가져와서 보여준다
-'''
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-@app.route('/')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(10000), nullable=False)
+
+    def __repr__(self):
+        return f'{self.username} {self.title} 추천 by {self.username}'
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
 def home():
-        name='JI'
-        motto = '최선을 다해 재밌게!'
+    name = '최지웅'
+    motto = "행복해서 웃는게 아니라 웃어서 행복합니다."
 
+    context = {
+        "name": name,
+        "motto": motto,
+    }
+    return render_template('motto.html', data=context)
 
-
-        context = {
-            'name': name,
-            'motto': motto,
-        }
-        return render_template('motto.html',data=context)
-
-@app.route('/music')
+@app.route("/music/")
 def music():
-        return render_template('music.html')
+    song_list = Song.query.all()
+    return render_template('music.html', data=song_list)
 
-@app.route('/iloveyou/<name>/')
+@app.route("/music/<username>")
+def render_music_filter(username):
+    filter_list = Song.query.filter_by(username=username).all()
+    return render_template('music.html', data=filter_list)
+
+
+@app.route("/iloveyou/<name>/")
 def iloveyou(name):
-        motto = f"{name}야 난 너뿐이야!"
-        context = {
-               'name': name,
-               'motto': motto
-        }
-        return render_template('motto.html', data=context)
+    motto = f"{name}야 난 너뿐이야!"
+
+    context = {
+        'name': name,
+        'motto': motto,
+    }
+    return render_template('motto.html', data=context)
+
+@app.route('/music/create')
+def music_create():
+    username_receive = request.args.get('username')
+    title_receive = request.args.get('title')
+    artist_receive = request.args.get('artist')
+    image_url_receive = request.args.get('image_url')
+
+        #DB에 데이터 저장
+    song= Song(username=username_receive, title=title_receive, artist=artist_receive, image_url=image_url_receive)
+    db.session.add(song)
+    db.session.commit()
+    return redirect(url_for('render_music_filter', username=username_receive))
 
 if __name__ == "__main__":
     app.run(debug=True)
